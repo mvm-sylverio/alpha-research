6f4adecff1ef4473c46479587ae4a7af8fc08944
Marcos Vinicius Sylverio <visylvs@gmail.com>
2026-09-03 22:17:41 -0300

    fix(tests): create test modules with inits and add pytest setup in toml to fix same name script problem in tests

---
+++ alpha_research/visualization/__init__.py
@@ -0,0 +1,13 @@
+from alpha_research.visualization.features import (
+    plot_cross_sectional_value_summary,
+    plot_time_series_value,
+)
+from alpha_research.visualization.timeseries import (
+    plot_rolling_temporal_association,
+)
+
+__all__ = [
+    'plot_time_series_value',
+    'plot_cross_sectional_value_summary',
+    'plot_rolling_temporal_association',
+]
